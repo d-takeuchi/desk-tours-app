42630a068a31fdc5f04d608e1eb4c52915cf0db3
takeuchi <kuronk.kuro3@gmail.com>
2021-12-15 15:03:14 +0900

    微修正

--- client/src/components/pages/auth/Login.tsx
+++ client/src/components/pages/auth/Login.tsx
@@ -92,12 +92,13 @@ export const Login: VFC = () => {
             <button
               type="button"
               className="inline-flex w-full px-4 py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-white border rounded-lg border-blueGray-300 hover:bg-gray-400 hover:text-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 mt-5"
+              onClick={() =>
+                (window.location.href = `${process.env.REACT_APP_API_URL}/auth/twitter`)
+              }
             >
               <div className="flex items-center justify-center w-full">
                 <TwitterIcon />
-                <a href={`${process.env.REACT_APP_API_URL}/auth/twitter`}>
-                  <span className="ml-4">Twitterアカウントでログイン</span>
-                </a>
+                <span className="ml-4">Twitterアカウントでログイン</span>
               </div>
             </button>
           </div>

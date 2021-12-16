698b9aba576823767e576a77e0f08a55d52a7e5b
takeuchi <kuronk.kuro3@gmail.com>
2021-12-16 20:03:19 +0900

    画像保存処理の微修正

--- server/src/photo/photo.service.ts
+++ server/src/photo/photo.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import * as urlsafeBase64 from 'urlsafe-base64'
-
-import { Post } from 'src/posts/post.entity'
+import * as isBase64 from 'is-base64'
 
 @Injectable()
 export class PhotoService {
-  public async uploadPhoto(post: Post, imageFile: string) {
+  public async uploadPhoto(key: string, imageFile: string) {
+    if (!isBase64(imageFile, { allowMime: true })) {
+      return imageFile
+    }
+
     const decode_data = urlsafeBase64.decode(
       imageFile.replace('data:image/jpeg;base64,', '')
     )
@@ -14,14 +17,14 @@ export class PhotoService {
     const s3Client = new S3Client({ region: process.env.AWS_REGION })
     const uploadParams = {
       Bucket: process.env.AWS_S3_BUCKET,
-      Key: `${post.id}.jpg`,
+      Key: `${key}.jpg`,
       Body: decode_data,
       ContentType: 'image/jpeg',
     }
     try {
       await s3Client.send(new PutObjectCommand(uploadParams))
 
-      return this.getPhotoUrl(`${post.id}.jpg`)
+      return this.getPhotoUrl(`${key}.jpg`)
     } catch (err) {
       console.log(err)
       throw new Error()
